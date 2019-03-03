4315595d0ff3542e1894e3a6ee6cdae32f0a8f6e
Antonio Jack <ict2jack@gmail.com>
2019-03-03 16:45:06 +0700

    Format code

--- src/components/CardofInput/CardofInput.js
+++ src/components/CardofInput/CardofInput.js
@@ -19,11 +19,11 @@ class Label extends React.Component {
         let countArray = [0] //keep numbers of square blacket
         let leafIndexArray = []
         let treeData = [
-    {
-        name: ''
-    },
+            {
+                name: ''
+            },
 
-];
+        ];
         let array = this.state.label.split('[');
         console.log(array);
 
@@ -66,10 +66,10 @@ class Label extends React.Component {
             console.log('count:', countArray);
 
             var nodeArray = [];
-            for (var i=0 ; i< drawArray.length  ; i++){
-                nodeArray[i] = drawArray[i].replace(/]/g,'');
+            for (var i = 0; i < drawArray.length; i++) {
+                nodeArray[i] = drawArray[i].replace(/]/g, '');
             }
-            
+
             if (countArray.length == 1) {
                 var cnode = treeData[0];
                 cnode.name = nodeArray[0]
@@ -104,7 +104,7 @@ class Label extends React.Component {
                 }
 
             }
-            
+
             let jsonz = JSON.stringify(treedata);
             console.log('Tree', jsonz);
 
@@ -135,87 +135,87 @@ class Label extends React.Component {
     }
 
 
-        // handleDraw = () => {
-        //     this.props.handlerFromParent(this.state.label);
-        // }
+    // handleDraw = () => {
+    //     this.props.handlerFromParent(this.state.label);
+    // }
 
-        addSentence = (event) => {
-            event.preventDefault();
-            const db = firebase.firestore();
-            db.collection('Treebank').add({
-                sentence: this.state.sentence,
-                label: this.state.label
-            });
-        };
+    addSentence = (event) => {
+        event.preventDefault();
+        const db = firebase.firestore();
+        db.collection('Treebank').add({
+            sentence: this.state.sentence,
+            label: this.state.label
+        });
+    };
 
-        SuccessAlert = () => {
-            if ((this.state.sentence != '') && (this.state.label != '')) {
-                alert("Successfully added to the database!");
-            }
-        }
-
-        updateInput = (event) => {
-            this.setState({
-                [event.target.name]: event.target.value
-            });
-        }
-
-        clearInput = () => {
-            this.setState({
-                sentence: '',
-                label: ''
-            });
-        }
-
-        render() {
-            return (
-                <div>
-                    {/* Title */}
-                    <Card body className="text-center">
-                        <CardBody>
-                            <CardTitle >Labelled Bracket </CardTitle>
-                        </CardBody>
-
-                        {/* Label Bracket textarea */}
-                        <form onSubmit={this.addSentence}>
-                            <textarea
-                                rows="5" cols="40"
-                                name="label"
-                                placeholder="[S(2)[NP[NCMN เมล็ดกาแฟ]] [VP(1)[VACT กระตุ้น][NP[NCMN หัวใจ]]]]"
-                                value={this.state.label}
-                                onChange={this.updateInput}
-                                required>
-                            </textarea>
-                            <br></br>
-
-                            {/* Sentence Textarea */}
-                            <textarea
-                                rows="3" cols="40"
-                                name="sentence"
-                                placeholder="เมล็ดกาแฟกระตุ้นหัวใจ"
-                                value={this.state.sentence}
-                                onChange={this.updateInput}
-                                required
-                            >
-                            </textarea>
-                            <br></br>
-                            <br></br>
-
-                            {/* Draw & Clear Buttons  */}
-                            <span>
-                                <Button onClick={this.extracttheInput} color="primary" size="sm">Draw</Button>{' '}
-                                <Button color="primary" size="sm" onClick={this.clearInput}>Clear</Button>
-                            </span>
-                            <br></br>
-                            <br></br>
-
-                            {/* Add to database button */}
-                            <Button type="submit" color="primary" size="lg" active
-                                onClick={this.SuccessAlert} >Add to database</Button>
-                        </form>
-                    </Card>
-                </div>
-            );
+    SuccessAlert = () => {
+        if ((this.state.sentence != '') && (this.state.label != '')) {
+            alert("Successfully added to the database!");
         }
     }
-    export default Label;
+
+    updateInput = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    clearInput = () => {
+        this.setState({
+            sentence: '',
+            label: ''
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {/* Title */}
+                <Card body className="text-center">
+                    <CardBody>
+                        <CardTitle >Labelled Bracket </CardTitle>
+                    </CardBody>
+
+                    {/* Label Bracket textarea */}
+                    <form onSubmit={this.addSentence}>
+                        <textarea
+                            rows="5" cols="40"
+                            name="label"
+                            placeholder="[S(2)[NP[NCMN เมล็ดกาแฟ]] [VP(1)[VACT กระตุ้น][NP[NCMN หัวใจ]]]]"
+                            value={this.state.label}
+                            onChange={this.updateInput}
+                            required>
+                        </textarea>
+                        <br></br>
+
+                        {/* Sentence Textarea */}
+                        <textarea
+                            rows="3" cols="40"
+                            name="sentence"
+                            placeholder="เมล็ดกาแฟกระตุ้นหัวใจ"
+                            value={this.state.sentence}
+                            onChange={this.updateInput}
+                            required
+                        >
+                        </textarea>
+                        <br></br>
+                        <br></br>
+
+                        {/* Draw & Clear Buttons  */}
+                        <span>
+                            <Button onClick={this.extracttheInput} color="primary" size="sm">Draw</Button>{' '}
+                            <Button color="primary" size="sm" onClick={this.clearInput}>Clear</Button>
+                        </span>
+                        <br></br>
+                        <br></br>
+
+                        {/* Add to database button */}
+                        <Button type="submit" color="primary" size="lg" active
+                            onClick={this.SuccessAlert} >Add to database</Button>
+                    </form>
+                </Card>
+            </div>
+        );
+    }
+}
+export default Label;
