6f19063ecc31173562e7f5528c791d7a30766ff0
Antonio Jack <ict2jack@gmail.com>
2019-04-08 20:09:30 +0700

    Add onclick handler and toggle the button.

--- src/components/SelectionButton.js
+++ src/components/SelectionButton.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Button } from 'reactstrap';
 
 export default class Buttons extends React.Component {
-    render() {
-      return (
-        <div class = "pt-5"  > 
-          
-          <Button outline color="secondary"  >Dependency Tree</Button>
-          
-        </div>
-      );
-    }
+  state = {
+    toggle: true
   }
+
+  handleClick = () => {
+    this.setState({ toggle: !this.state.toggle });
+  }
+
+  render() {
+    const btnValue = this.state.toggle ? "Constituent Tree" : "Dependency Tree"
+
+    return (
+      <div class="pt-5">
+        <Button outline color="secondary" onClick={this.handleClick} >{btnValue}</Button>
+      </div>
+    );
+  }
+}
