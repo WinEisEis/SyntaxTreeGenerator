e6181f35f9defc35d7ad96ea2c14df7424cc80be
unknown <xjellomasterx@gmail.com>
2019-04-24 01:31:41 +0700

    Add DrawDependencyTree function

--- src/components/CardofInput/CardofInput.js
+++ src/components/CardofInput/CardofInput.js
@@ -7,7 +7,7 @@ import firebase from '../../Firebase';
 
 class Label extends React.Component {
     state = {
-        sentence: 'ฉันกินข้าว',
+        sentence: '',
         label: ''
     };
 
@@ -29,50 +29,65 @@ class Label extends React.Component {
     }
 
     getLabel = (label) => {
+
+
+
+
+        // [S(2)[NP[NCMN เมล็ดกาแฟ]][VP(1)[VACT กระตุ้น][NP[NCMN หัวใจ]]]]
+
         let mainArray = [];
         let leafIndex = 0;
         let drawArray = [];
         let count = 0;
-        let index = 1;
+        let index = 0;
+        let id = 0;
+        let depth = 0;
         console.log("Initial leafindex", leafIndex);
+        var stringify = require('json-stringify-safe');
 
-        //[S(2)[NP[NCMN เมล็ดกาแฟ]] [VP(1)[VACT กระตุ้น][NP[NCMN หัวใจ]]]]
-        //[S [NP [NCMN เมล็ด] [NP [NCMN กาแฟ]]] [VP [VACT กระตุ้น] [NP [NCMN หัวใจ]]]]
-        // const label = "[S(2)[NP[NCMN Ɛ]] [VP(1) [VACT ส่งเสริม] [NP[NCMN อาชีพ]] [PP(1) [RPRE ให้] [NP[NCMN ประชาชน]] [VP(1,2) [NEG ไม่][VACT ให้เกิด] [NP(2) [FIXN การ] [VSTA ว่างงาน]]]]]]";
-        const label1 = "[S [NP [PPRS ฉัน]] [VP [VACT กิน] [NP [NCMN ข้าว]]]]"
 
-        //1. split "["
+        // const label = "[S(2)[NP[NCMN Ɛ]] [VP(1) [VACT ส่งเสริม] [NP[NCMN อาชีพ]] [PP(1) [RPRE ให้] [NP[NCMN ประชาชน]] [VP(1,2)[NEG ไม่][VACT ให้เกิด] [NP(2)[FIXN การ] [VSTA ว่างงาน]]]]]]";
+        
         let array = label.split('[');
-
-        console.log("อาเรคือที่ยังไม่ทริม", array);
-
-        array = array.map(str => str.trim());
-        console.log("อาเรทริมแล้ว", array);
+        console.log(array);
 
         //2. Push to mainArray
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] !== "," & array[i] !== "") {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] != "," & array[i] != "") {
                 mainArray.push(array[i]);
             }
         }
         console.log(mainArray);
 
-        let countArray = [0]//keep numbers of square bracket
+        // Output we got looks like this
+        // 0: "S(2)"
+        // 1: "NP"
+        // 2: "NCMN เมล็ดกาแฟ]]"
+        // 3: "VP(1)"
+        // 4: "VACT กระตุ้น]"
+        // 5: "NP"
+        // 6: "NCMN หัวใจ]]]]"
+        // length: 7
+        // __proto__: Array(0)
+
+        // 3.1 Iterate over mainArray
+        //3.2  ถ้าเจอ "]" ก็ หาindexของจุดที่เจอ แล้วจึงbreak
+        let countArray = [-1]//keep numbers of square bracket
         let leafIndexArray = []
 
-        let treeData1 = [
+        let treeData = [
             {
                 name: ''
             },
 
         ];
 
-        while (mainArray.length !== 0) {
-            // for (let i = 0; i>2; i++){
+        while (mainArray.length != 0) {
+            // for (var i = 0; i>2; i++){
             dance:
-            for (let i = 0; i < mainArray.length; i++) {
-                for (let j = 0; j < mainArray[i].length; j++) {
-                    if (mainArray[i][j] === "]") {
+            for (var i = 0; i < mainArray.length; i++) {
+                for (var j = 0; j < mainArray[i].length; j++) {
+                    if (mainArray[i][j] == "]") {
                         leafIndex = i;
                         break dance;
                     }
@@ -84,7 +99,7 @@ class Label extends React.Component {
             //4. draw  0: "S(2)"  1: "NP" 2: "NCMN เมล็ดกาแฟ]]"
 
 
-            for (let i = 0; i <= leafIndex; i++) {
+            for (var i = 0; i <= leafIndex; i++) {
                 drawArray.push(mainArray[i]);
 
             }
@@ -96,25 +111,31 @@ class Label extends React.Component {
 
             // find and travese the node that already have been constructed
             console.log('count:', countArray);
-            // let nodeArray = drawArray.slice(0);//clone draw array
+            // var nodeArray = drawArray.slice(0);//clone draw array
             // remove ]] from string
 
-            let nodeArray = [];
-            for (let i = 0; i < drawArray.length; i++) {
+            var nodeArray = [];
+            for (var i = 0; i < drawArray.length; i++) {
                 nodeArray[i] = drawArray[i].replace(/]/g, '');
             }
             console.log('nodeArray', nodeArray);
 
-            let cnode;
-            if (countArray.length === 1) {
-                cnode = treeData1[0];
-                cnode.name = nodeArray[0]
+            if (countArray.length == 1) {
+                var cnode = treeData[0];
+                cnode.name = nodeArray[0];
+                cnode.depth = depth;
+                depth++;
+                id++;
+                
+                cnode.id = id;
+                let m = parseInt(cnode.name[cnode.name.length - 2]);// retrieve header  
+                cnode.header = m;
                 nodeArray.shift();//remove first element of the array
             }
             else {
-                cnode = treeData1[0];
+                var cnode = treeData[0];
                 nodeArray.shift();
-                for (let i = 0; i < leafIndexArray[leafIndexArray.length - 1] - countArray[countArray.length - 1]; i++) {//retrieve the right most element of the countArray
+                for (var i = 0; i < leafIndexArray[leafIndexArray.length - 1] - countArray[countArray.length - 1]; i++) {//retrieve the right most element of the countArray
                     cnode = cnode.children[cnode.children.length - 1];// select the right most node
                     nodeArray.shift();
                 }
@@ -123,38 +144,78 @@ class Label extends React.Component {
             console.log("current node", cnode)
             console.log("to draw node", nodeArray)
             // put into JSON
-            for (let i = 0; i < nodeArray.length; i++) {
+            depth = cnode.depth;
+            for (var i = 0; i < nodeArray.length; i++) {
                 // console.log(cnode.children)
+
+
                 if (cnode.children === undefined) {//when node has no child
                     cnode.children = [];//create children array
                     cnode.children[0] = {};//create object in children array
                     cnode = cnode.children[0];//traverse down the tree
                     cnode.name = nodeArray[i];//add name
-                    // if (i == nodeArray.length - 1) {//add index to child node
-                    //     console.log("test")
-                    //     cnode.index = index;
-                    //     index++;
-                    // }
+                    depth++;
+                    cnode.depth = depth;
+                    id++;
+                    cnode.id = id;//add ID
+
+                    if (i == nodeArray.length - 1) {//add index to child node
+                        cnode.index = index;
+                        index++;
+                        cnode.header = 0;
+                    }
+
+                    else {
+                        if (isNaN(cnode.name[cnode.name.length - 2]) === false) {//check wether header exist if yes
+                            let m = parseInt(cnode.name[cnode.name.length - 2]);// retrieve header  
+                            cnode.header = m;
+                        }
+
+                        else {
+                            cnode.header = 1;
+                        }
+                    }
+
                 }
                 else {// node already has a child
-                    let nodelength = cnode.children.length;
+                    var nodelength = cnode.children.length;
                     cnode.children[nodelength] = {};
                     cnode = cnode.children[nodelength];
                     cnode.name = nodeArray[i];
+                    depth++;
+                    cnode.depth = depth;
+                    id++;
+                    cnode.id = id;//add ID
 
+
+                    if (i == nodeArray.length - 1) {//add index to child node
+                        cnode.index = index;
+                        index++;
+                        cnode.header = 0;
+                    }
+
+                    else {
+                        if (isNaN(cnode.name[cnode.name.length - 2]) === false) {//check wether header exist if yes
+                            let m = parseInt(cnode.name[cnode.name.length - 2]);// retrieve header                
+                            cnode.header = m;
+                        }
+                        else {
+                            cnode.header = 1;
+                        }
+                    }
                 }
 
 
 
             }
 
-            console.log('Tree', JSON.stringify(treeData1));
+            console.log('Tree', stringify(treeData));
 
             //5. Count "]" in the drawArray
             let Pop = drawArray[leafIndex];
             console.log(Pop);
-            for (let i = 0; i < Pop.length; i++) {
-                if (Pop[i] === "]") {
+            for (var i = 0; i < Pop.length; i++) {
+                if (Pop[i] == "]") {
                     count++;
                 }
             }
@@ -170,29 +231,20 @@ class Label extends React.Component {
             drawArray = []
 
             console.log('result array:', mainArray);
-            function jsonCopy(src) {
-                return JSON.parse(JSON.stringify(src));
-            }
-            const source = { a: 1, b: 2, c: 3 };
-            const target = jsonCopy(source);
-            console.log(target); // {a:1, b:2, c:3}
-            let treeData = jsonCopy(treeData1);
-            this.props.handlerFromParent(treeData);
         }
+      
 
 
-        console.log("Number of elements to be popped:", count);
 
-        countArray.push(count);//appends count
-        leafIndexArray.push(leafIndex);//appends count
 
-        mainArray.splice(leafIndex - count + 1, count);
-        count = 0;
-        leafIndex = -1;
-        drawArray = []
 
-        console.log('result array:', mainArray);
-        this.props.handlerFromParent(treeData1);
+
+
+
+
+
+
+        this.props.handlerFromParent(treeData);
     }
 
     addSentence = (event) => {
@@ -227,7 +279,7 @@ class Label extends React.Component {
                 {/* Title */}
                 <Card body className="text-center">
                     <CardBody>
-                        <CardTitle >Labelled Bracket </CardTitle>
+                        <CardTitle >Labelled Bracket & Sentence</CardTitle>
                     </CardBody>
 
                     {/* Label Bracket textarea */}
@@ -235,7 +287,7 @@ class Label extends React.Component {
                         <textarea
                             rows="5" cols="40"
                             name="label"
-                            placeholder="[S(2)[NP[NCMN เมล็ดกาแฟ]] [VP(1)[VACT กระตุ้น][NP[NCMN หัวใจ]]]]"
+                            placeholder="Input Labelled bracket here,e.g.,[S(2)[NP[NCMN เมล็ดกาแฟ]] [VP(1)[VACT กระตุ้น][NP[NCMN หัวใจ]]]]"
                             value={this.state.label}
                             onChange={this.updateInput}
                             required>
@@ -246,7 +298,7 @@ class Label extends React.Component {
                         <textarea
                             rows="3" cols="40"
                             name="sentence"
-                            placeholder="ฉันกินข้าว"
+                            placeholder="Input raw sentence here,e.g., มะขามใช้เป็นยาระบาย"
                             value={this.state.sentence}
                             onChange={this.updateInput}
                             required
